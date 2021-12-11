1002ff0f4769f0b2bb51f59d52c2eee0dda67f19
Diyorbek0617 <nizomiddinov.d.d@gmail.com>
2021-12-11 12:39:18 +0500

    name make to Full screen type of sharp

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 class Home_page extends StatefulWidget {
   const Home_page({Key? key}) : super(key: key);
@@ -13,6 +14,7 @@ class _Home_pageState extends State<Home_page> {
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
+          // #gradient to Full screen
           gradient: LinearGradient(
            begin: Alignment.topCenter,
             colors: [
@@ -30,6 +32,7 @@ class _Home_pageState extends State<Home_page> {
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
             SizedBox(height: 100,),
+            //#Sign Up text
             Padding(
               padding:  EdgeInsets.only(right: 20.0),
               child: Align(
@@ -38,6 +41,7 @@ class _Home_pageState extends State<Home_page> {
                   child: Text("Sign Up",style: TextStyle(color: Colors.white,fontSize:40,),)),
             ),
             SizedBox(height: 10,),
+            //#Welcome text
             Padding(
               padding: EdgeInsets.only(right: 20.0),
               child: Align(
@@ -57,27 +61,33 @@ class _Home_pageState extends State<Home_page> {
                     child: Column(
                       children: [
                         SizedBox(height: 50,),
+                        // #textfield
                         Container(
                           padding: EdgeInsets.all(5),
-
                           decoration: BoxDecoration(
-
-                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.white,
+                            borderRadius: BorderRadius.horizontal( left:Radius.circular(10.0), right:Radius.circular(10.0),
+                            ),
                             boxShadow: [
                               BoxShadow(color: Color.fromRGBO(171, 171, 171, 0.7),blurRadius:20,offset: Offset(0,10)),
                             ],
                           ),
+                          //#textfield type
                           child: Column(
                             children: [
+                              //#fullname
                               Container(
+                                decoration:BoxDecoration(
+                                  color:Colors.white,
+                                ),
                                 child: TextField(
                                   decoration: InputDecoration(
                                    hintStyle: TextStyle(color: Colors.grey,),
                                    hintText: "Full name"
                                   ),
                                 ),
-                                color: Colors.white,
                               ),
+                              //#email
                               Container(
                                 child: TextField(
                                   decoration: InputDecoration(
@@ -87,6 +97,7 @@ class _Home_pageState extends State<Home_page> {
                                 ),
                                 color: Colors.white,
                               ),
+                              //#phone
                               Container(
                                 child: TextField(
                                   decoration: InputDecoration(
@@ -96,6 +107,7 @@ class _Home_pageState extends State<Home_page> {
                                 ),
                                 color: Colors.white,
                               ),
+                              //#password
                               Container(
                                 child: TextField(
                                   decoration: InputDecoration(
@@ -111,6 +123,7 @@ class _Home_pageState extends State<Home_page> {
                           ),
                         ),
                         SizedBox(height: 20,),
+                        //#sign up
                         Container(
                           margin: EdgeInsets.symmetric(horizontal: 40.0),
                           height: 50,
@@ -124,6 +137,7 @@ class _Home_pageState extends State<Home_page> {
                           ),
                         ),
                         SizedBox(height: 20,),
+                        //text
                         Align(
                           alignment: Alignment.center,
                           child: Text("Sign Up with SNS",style: TextStyle(
@@ -131,6 +145,7 @@ class _Home_pageState extends State<Home_page> {
                           ),),
                         ),
                         SizedBox(height: 20,),
+                        // #Facebook #google #github
                         Container(
                           height: 60,
                           child: Row(
